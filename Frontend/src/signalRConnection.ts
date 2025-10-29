c2627d1311797f3a8f716e6d0f96549179a4fdf5
Sebastian Kvist <sebastian_kvist@hotmail.com>
2025-10-29 20:38:03 +0100

    remove hard coded local signal r

--- Frontend/src/signalRConnection.ts
+++ Frontend/src/signalRConnection.ts
@@ -1,10 +1,12 @@
 import * as signalR from "@microsoft/signalr";
 
 const token = localStorage.getItem("token");
+const apiBaseUrl = import.meta.env.VITE_API_URL ?? "";
+const hubUrl = `${apiBaseUrl.replace(/\/api$/, "")}/bookingHub`;
 
 export const connection = new signalR.HubConnectionBuilder()
-  .withUrl("http://localhost:5022/bookingHub", {
-    accessTokenFactory: () => token || ""
+  .withUrl(hubUrl, {
+    accessTokenFactory: () => token || "",
   })
   .withAutomaticReconnect()
   .build();
